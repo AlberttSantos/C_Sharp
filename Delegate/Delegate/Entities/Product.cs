cd084acd0f75e3de5a5644d951926e901a59a83e
Albertt Santos <alberttaurelio@gmail.com>
2018-10-03 22:29:21 -0300

    Delegates Preducate and Action - Aula 133

---
+++ Delegate/Delegate/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate.Entities
+{
+    class Product
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Product(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Preco;
+        }
+    }
+}
--- Delegate/Delegate/Program.cs
+++ Delegate/Delegate/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Delegate.Services;
+using Delegate.Entities;
+
 
 namespace Delegate
 {
@@ -26,14 +28,68 @@ namespace Delegate
             //Console.WriteLine(op_max(a, b));
 
             //*********** Multicast Delegates - Aula 131 ***************
-            double a = 7;
-            double b = 5;
+            //double a = 7;
+            //double b = 5;
 
-            OperacaoNumeriaBinariaVoid op = CalculationService.ShowSum; // O delegate recebe a referencia do metodo "CalculationService.Sum"
-            op += CalculationService.ShowMax; //Adiciona mais uma referencia no delegate
+            //OperacaoNumeriaBinariaVoid op = CalculationService.ShowSum; // O delegate recebe a referencia do metodo "CalculationService.Sum"
+            //op += CalculationService.ShowMax; //Adiciona mais uma referencia no delegate
 
-            op(a, b); //Chama as duas referencias na ordem
+            //op(a, b); //Chama as duas referencias na ordem
 
+
+
+            //*********** Delegate Predicate - Aula 132 ***************
+            //List<Product> list = new List<Product>();
+
+            //list.Add(new Product("TV", 1700.00));
+            //list.Add(new Product("Celular", 1200.00));
+            //list.Add(new Product("Mouse", 30.00));
+            //list.Add(new Product("Teclado", 89.90));
+
+            //////Resolução com função lambda
+            ////list.RemoveAll(produto => produto.Preco >= 100); // "=>" tal que (Remove todos produtos maior ou igual a cem)
+            ////foreach (Product p in list)
+            ////{
+            ////    Console.WriteLine(p);
+            ////}
+
+            ////Resolução com Predicate
+            //list.RemoveAll(ProductTest); //Remove os elementos da lista que a função retorna como verdadeiro
+            //foreach (Product p in list)
+            //{
+            //    Console.WriteLine(p);
+            //}
+
+
+
+            //*********** Delegate Action - Aula 133 ***************
+            List<Product> lista = new List<Product>();
+
+            lista.Add(new Product("TV", 300.00));
+            lista.Add(new Product("Celular", 100.00));
+            lista.Add(new Product("Mouse", 30.00));
+            lista.Add(new Product("Teclado", 89.90));
+
+            //Exemplo declarando um Action
+            Action<Product> act = p => { p.Preco += p.Preco * 0.1; }; //Utiliza-se as chaves para mostrar que é do tipo "Void"
+
+            lista.ForEach(act); //Percorre a lista e executa o "UpdatePrice"
+            foreach (Product p in lista)
+            {
+                Console.WriteLine(p);
+            }
         }
+
+        //Resolução com Predicate
+        public static bool ProductTest(Product p) //Função Predicate (Recebe um obejeto e retorna um boleano)
+        {
+            return p.Preco >= 100;
+        }
+
+        //Resolução Action
+        //public static void UpdatePrice(Product p)
+        //{
+        //    p.Preco += p.Preco * 0.1; // += recebe oq já tinha
+        //}
     }
 }
